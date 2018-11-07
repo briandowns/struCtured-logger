acd432b96e44918686f6202d0889acab6a7e8c43
Brian Downs <brian.downs@gmail.com>
2018-11-07 14:43:07 -0700

    add one more test

--- tests/tests.c
+++ tests/tests.c
@@ -21,12 +21,19 @@ void test_log_three_fields(void) {
     TEST_ASSERT_EQUAL_INT(110, wc);
 }
 
+void test_log_odd_count_fields(void) {
+    slog_init(stdout);
+    int wc = slog(SLOG_DEBUG, "field1", slog_string("value1"), slog_double(3.14), "field3", slog_int64(89));
+    TEST_ASSERT_EQUAL_INT(66, wc);
+}
+
 int main(int argc, char **argv) {
     UNITY_BEGIN();
 
     RUN_TEST(test_slog_init);
     RUN_TEST(test_log_two_fields);
     RUN_TEST(test_log_three_fields);
+    RUN_TEST(test_log_odd_count_fields);
 
     return UNITY_END();
 }
