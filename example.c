451858143df71c38bf79ae80bf42dd31afde73f0
Brian Downs <brian.downs@gmail.com>
2019-06-17 09:28:13 -0700

    update example

--- example.c
+++ example.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
-#include "src/slog.h"
+#include "log.h"
 
 int main(int argc, char **argv) {
     slog_init(stdout);
-    slog(SLOG_INFO, "msg", slog_string("records added successfully"), "count", slog_int(2));
-    slog(SLOG_INFO, "msg", slog_string("records added successfully"), "count", slog_int64(9223372036854775807));
-    slog(SLOG_INFO, "msg", slog_string("record added successfully"), "name", slog_string("Brian"), "elapsed", slog_double(5.76));
+    slog(LOG_INFO, "msg", log_string("records added successfully"), "count", log_int(2));
+    slog(LOG_INFO, "msg", log_string("records added successfully"), "count", log_int64(9223372036854775807));
+    slog(LOG_INFO, "msg", log_string("record added successfully"), "name", log_string("Brian"), "elapsed", slog_double(5.76));
 }
